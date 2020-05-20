3bdb94b01ecbc1e68943020c9efa22ebb3bbb0cd
Drew Beckmen <drew.beckmen@yale.edu>
2020-05-20 18:13:21 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -28,12 +28,21 @@ def initial_round
   total
 end
 
-def hit?
-  # code hit? here
+def hit?(total)
+  prompt_user
+  input = get_user_input
+  if input == 'h'
+    total += deal_card
+  end
+  if input != 'h' && input != 's'
+    invalid_command
+    prompt_user
+  end
+  total
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
